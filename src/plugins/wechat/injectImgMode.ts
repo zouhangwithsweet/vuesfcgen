dfa5dc4b34dd38b8d85b6f63ad5d346e6fb41077
zouhang <zouhang@didiglobal.com>
2020-06-19 19:05:49 +0800

    feat(plugins): inject props

---
+++ src/plugins/wechat/injectImgMode.ts
@@ -0,0 +1,18 @@
+import { AttributeNode, NodeTypes, locStub } from '@vue/compiler-core'
+
+export function injectImgMode(options: {
+  [k: string]: string
+} = {
+  mode: 'withFix'
+}): AttributeNode[] {
+  return Object.keys(options).map(k => ({
+    name: k,
+    type: NodeTypes.ATTRIBUTE,
+    loc: locStub,
+    value: {
+      content: options[k],
+      loc: locStub,
+      type: NodeTypes.TEXT,
+    }
+  }))
+}
