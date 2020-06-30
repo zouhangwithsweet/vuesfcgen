e22d7a7a8f399421cc9a663dee0b06b9eb1a5f73
zouhang <zouhang@didiglobal.com>
2020-06-30 10:56:00 +0800

    build: build plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,58 @@
 import commonjs from 'rollup-plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
-// import path from 'path'
+
+const external = [
+  '@vue/compiler-core',
+  '@babel/core',
+  'postcss',
+  'prettier',
+  'lodash/cloneDeep'
+]
+
+const plugins = [
+  commonjs(),
+  resolve(),
+]
+
+const srcInput = './src/index.ts'
+const pluginsInput = './src/plugins/wechat/index.ts'
 
 export default [
-  // esm module
+  // esm
   {
-    input: './src/index.ts',
+    input: srcInput,
     output: {
       format: 'esm',
+      dir: 'lib-module'
+    },
+    plugins: [
+      typescript({
+        declaration: true,
+        declarationDir: 'lib-module/types',
+        sourceMap: false,
+      }),
+      ...plugins,
+    ],
+    external,
+  },
+  {
+    input: pluginsInput,
+    output: {
+      format: 'esm',
+      dir: 'lib-module/plugins/wechat'
+    },
+    plugins: [
+      typescript({ sourceMap: false, }),
+      ...plugins,
+    ],
+    external,
+  },
+  // cjs
+  {
+    input: srcInput,
+    output: {
+      format: 'cjs',
       dir: 'lib'
     },
     plugins: [
@@ -17,15 +61,20 @@ export default [
         declarationDir: 'lib/types',
         sourceMap: false,
       }),
-      commonjs(),
-      resolve(),
+      ...plugins,
     ],
-    external: [
-      '@vue/compiler-core',
-      '@babel/core',
-      'postcss',
-      'prettier',
-      'lodash/cloneDeep'
-    ]
+    external,
+  },
+  {
+    input: pluginsInput,
+    output: {
+      format: 'cjs',
+      dir: 'lib-module/plugins/wechat'
+    },
+    plugins: [
+      typescript({ sourceMap: false, }),
+      ...plugins,
+    ],
+    external,
   },
 ]
